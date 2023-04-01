e6211a94fefac91918e6563a2173169fa15df360
root <midnightlunalux@gmail.com>
2023-04-01 16:46:34 -0400

    committing all files

---
+++ wordfinder.py
@@ -0,0 +1,38 @@
+"""Word Finder: finds random words from a dictionary."""
+import random
+
+class WordFinder:
+    ...
+    def __init__(self, path):
+        """Read dictionary and reports # items read."""
+
+        dict_file = open(path)
+
+        self.words = self.parse(dict_file)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words."""
+
+        return [w.strip() for w in dict_file]
+    
+    def random(self):
+        """Return random word."""
+
+        return random.choice(self.words)
+    
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments."""
+
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words, skipping blanks/comments."""
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
+    
+
+"""
+Honestly being away from coding for so long and already being pretty unfamiliar with python, this section was a bit of a trip for me but I think I'm starting to familiarize/refamiliarize a bit. 
+"""
